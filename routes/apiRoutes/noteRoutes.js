6b43e7a28e3e443c4550f066f4c2e693885a234a
ckyle121 <carolinekyle21@gmail.com>
2022-02-23 12:28:31 -0600

    add heroku

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { getAndRenderNotes } = require('../../public/assets/js/index');
+const { notes } = require('../../db/db.json');
+
+// GET request for notes stored in json db
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+// POST request for notes stored in json db 
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toStirng();
+
+    const results = getAndRenderNotes(req.body, notes);
+    res.json(results);
+});
